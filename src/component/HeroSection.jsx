e9398238e4b2e48a7b2629f72022186e3bf5dd77
Saswata Paul <saspaul2014@gmail.com>
2024-01-13 02:07:09 +0530

    added the component folder and added the basic navbar function abd the hero section

---
+++ src/component/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function HeroSection() 
+{
+    return (
+        <div className="hero-section">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+                quidem, quod.
+            </p>
+            <h1>Welcome to the world of coding</h1>
+        </div>
+  );
+}
+
+export default HeroSection;
