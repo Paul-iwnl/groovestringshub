949bcfabb956290b50520f693fd0e2041e2c47ca
Saswata Paul <saspaul2014@gmail.com>
2024-01-15 04:47:35 +0530

    added the photogallery jsx file as well as the css file

---
+++ src/component/PhotoGallery.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/photogallery.css"
+
+function PhotoGallery() 
+{
+    return (
+        <div className="photo-gallery">
+            <h1>Photo Gallery</h1>
+        </div>
+    )
+}
+
+export default PhotoGallery;
