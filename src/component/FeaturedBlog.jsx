b343cee63e9fe9a4d0918e2054241a90e05f3c29
Saswata Paul <saspaul2014@gmail.com>
2024-01-14 17:53:25 +0530

    added the featured blog section on the front page of the ui

--- src/component/FeaturedBlog.jsx
+++ src/component/FeaturedBlog.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import "../styles/featuredblog.css"
+import warwickBassPicture from "../utils/warwick_bass_picture.jpg";
 
 function FeaturedBlog()
 {
-    return(
-        <div>
-            <h1>Featured Blog</h1>
+    return (
+      <div className="featured-blog-outer-area">
+        <div className="featured-blog-inner-area">
+          <div className="featured-blog-name-area">
+            <p>FEATURED BLOG</p>
+          </div>
+          <div className="featured-blog-picture-area">
+            <img src={warwickBassPicture} alt="Warwick Bass" />
+          </div>
+          <div className="featured-blog-title-area">
+            <div className="featured-blog-title">
+              <h3>Warwick Thumb BO 5: Unleashing the Power of Precision</h3>
+            </div>
+            <div className="featured-blog-body">
+              <p>
+                Greetings fellow bass aficionados! Today, we're taking a closer
+                look at the Warwick Thumb BO 5, a bass guitar that stands tall
+                in the realm of precision and craftsmanship. Buckle up as we
+                explore the sonic wonders of this German-engineered masterpiece.
+              </p>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
 
 export default FeaturedBlog;
